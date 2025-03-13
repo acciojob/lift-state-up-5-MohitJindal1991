987c5b9997ea22522b0fe941b4ecd3c408ba013a
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-13 10:01:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
+import './../styles/App.css';
 
 import React from "react";
-import './../styles/App.css';
-import {useState} from 'react';
-import Child from './Child.jsx';
+import { useState } from "react";
+import Child from "./Child.jsx";
 const App = () => {
-
-  const [isLoggedIn,setIsLoggedIn]=useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-    <h1>Parent Component</h1>
-    {isLoggedIn?<p>You are Logged in!</p>:<Child state={setIsLoggedIn}/>}
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      {isLoggedIn ? <p>You are Logged in!</p> : <Child state={setIsLoggedIn} />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
