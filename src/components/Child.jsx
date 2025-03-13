c950b831ace1200e4ddf981f5ed2b1ac0ec02113
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-13 09:42:42 +0530

    Update Child.jsx

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
 
+function Child(props){
+
+  return (
+    <>
+      <div>
+      <label htmlFor="username">Username:</label>
+      <input type="text" id=""username"  />
+      </div>
+
+      <div>
+      <label htmlFor="pass">Password:</label>
+      <input type="password" id="pass" />
+      </div>
+
+      <button onClick={props.useState}>Login</button>
+    </>
+  )
+  
+}
+export default Child;
